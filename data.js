ca465f4a3562ec77b08a6b94025a1bc519d2ec0c
jhtn <jjjhonlinegmail.com>
2023-09-16 16:56:31 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,80 @@
+export default [
+  {
+    title:
+      "Você / Gostava tanto de você",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "1-voce_gostavaTantoDeVoce.mp3",
+  },
+  {
+    title:
+      "A minha alma",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "2-aMinhaAlma.mp3",
+  },
+  {
+    title:
+      "Tempo perdido",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "3-tempoPerdido.mp3",
+  },
+  {
+    title:
+      "O tempo não pára",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "4-oTempoNaoPara.mp3",
+  },
+  {
+    title:
+      "Primeiros erros",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "5-primeirosErros.mp3",
+  },
+  {
+    title:
+      "Sujeito de sorte",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "6-sujeitoDeSorte.mp3",
+  },
+  {
+    title:
+      "Telegrama",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "7-telegrama.mp3",
+  },
+  {
+    title:
+      "Beth Balanço",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "8-bethBalanco.mp3",
+  },
+  {
+    title:
+      "Pele negra",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "9-peleNegra.mp3",
+  },
+  {
+    title:
+      "Amor e fé",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "10-amorEfe.mp3",
+  },
+  {
+    title:
+      "Que país é esse?",
+    artist: "Dan e Ramon - Live do Insta 2023",
+    cover: "capa.jpg",
+    file: "11-quePaisEhEsse.mp3",
+  },
+
+];
